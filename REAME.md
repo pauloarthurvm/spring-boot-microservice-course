b298c607d9d4ec6303911ec0f3a48793ad0a28e9
Paulo Varassin <pauloarthurvm@hotmail.com>
2021-02-02 19:59:43 -0300

    Paging and Sorting

--- REAME.md
+++ REAME.md
@@ -6,3 +6,8 @@ Hypermedia-Driven API
         - /tours
         - /tours/{code}
     - POST, DELETE and UPDATE (PUT, PATCH)
+    
+Paging and Sorting
+    - Spring auto implement when Repositories extends Paging and Sorting
+    - GET http://localhost:8080/tours/search/findByTourPackageCode?code=CC&size=2&sort=title,asc
+    - GET http://localhost:8080/tours?page=1&size=3&sort=title,asc
--- src/main/java/com/paulovarassin/springbootmicroservice/repo/TourRepository.java
+++ src/main/java/com/paulovarassin/springbootmicroservice/repo/TourRepository.java
@@ -1,13 +1,13 @@
 package com.paulovarassin.springbootmicroservice.repo;
 
 import com.paulovarassin.springbootmicroservice.domain.Tour;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
-
-    List<Tour> findByTourPackageCode(@Param("code")String code);
+    Page<Tour> findByTourPackageCode(@Param("code")String code, Pageable pageable);
 
 }
