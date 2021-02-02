9c527c1f72af6b978a7d351840d7f81d31e6a074
Paulo Varassin <pauloarthurvm@hotmail.com>
2021-02-02 19:50:51 -0300

    Using build-in endpoints (README) and create new endpoint

---
+++ REAME.md
@@ -0,0 +1,8 @@
+Hypermedia-Driven API
+- Spring generate itself endpoints from Repositories:
+    - GET 
+        - /tourPackages 
+        - /tourPackages/{code}
+        - /tours
+        - /tours/{code}
+    - POST, DELETE and UPDATE (PUT, PATCH)
--- src/main/java/com/paulovarassin/springbootmicroservice/repo/TourRepository.java
+++ src/main/java/com/paulovarassin/springbootmicroservice/repo/TourRepository.java
@@ -2,6 +2,12 @@ package com.paulovarassin.springbootmicroservice.repo;
 
 import com.paulovarassin.springbootmicroservice.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TourRepository extends CrudRepository<Tour, Integer> {
+
+    List<Tour> findByTourPackageCode(@Param("code")String code);
+
 }
